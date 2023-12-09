e687ac986697046cfef8de7159416c10cc60f247
Benjamin Piwowarski <benjamin@piwowarski.fr>
2023-12-09 16:19:03 +0100

    Cleanup with pre-commit

--- mymodel/experiment.py
+++ mymodel/experiment.py
@@ -1,21 +1,15 @@
 import logging
 
-from experimaestro import experiment
-from xpmir.learning.optim import (
-    TensorboardService,
-)
 import xpmir.evaluation
-from xpmir.papers.cli import paper_command
 from xpmir.papers.results import PaperResults
 from configuration import MyModel
 from xpmir.experiments.ir import ir_experiment, ExperimentHelper
 
 logging.basicConfig(level=logging.INFO)
 
+
 @ir_experiment()
-def run(
-    helper: ExperimentHelper, cfg: MyModel
-) -> PaperResults:
+def run(helper: ExperimentHelper, cfg: MyModel) -> PaperResults:
     """My model"""
 
     # A model that can be loaded, i.e. what is
@@ -33,4 +27,3 @@ def run(
         evaluations=tests,
         tb_logs={"MyModel-RR@10": learner.logpath},
     )
-
